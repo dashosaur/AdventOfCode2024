987e7503aa843d9f501b45cf8f504be99fe18a1f
Dash Brittain <roxanne.brittain@gmail.com>
2024-11-30 22:41:01 -0800

    Switch to Swift Testing

--- AdventOfCodeTests/PuzzleTests.swift
+++ AdventOfCodeTests/PuzzleTests.swift
@@ -5,16 +5,18 @@
 //  Created by Dash on 11/29/20.
 //
 
-import XCTest
+import Testing
 
-class PuzzleTests: XCTestCase {
-
-    func testExample() {
-        XCTAssertEqual(Example().solve1(input: "12,16"), 5)
-        XCTAssertEqual(Example().solve2(input: "100756"), 50346)
+struct PuzzleTests {
+    
+    @Test
+    func puzzleExample() {
+        #expect(Example().solve1(input: "12,16") == 5)
+        #expect(Example().solve2(input: "100756") == 50346)
     }
     
-    func testAOC1() {
+    @Test
+    func puzzle1() {
         let puzzle = AOC1()
         
         let input1 = """
@@ -26,346 +28,370 @@ class PuzzleTests: XCTestCase {
         3   3
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 11)
+        #expect(puzzle.solve1(input: input1) == 11)
         
         let input2 = input1
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 31)
+        #expect(puzzle.solve2(input: input2) == 31)
     }
     
+    @Test
     func testAOC2() {
         let puzzle = AOC2()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC3() {
         let puzzle = AOC3()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC4() {
         let puzzle = AOC4()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC5() {
         let puzzle = AOC5()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC6() {
         let puzzle = AOC6()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC7() {
         let puzzle = AOC7()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC8() {
         let puzzle = AOC8()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC9() {
         let puzzle = AOC9()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC10() {
+    @Test
+    func puzzle10() {
         let puzzle = AOC10()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC11() {
+    @Test
+    func puzzle11() {
         let puzzle = AOC11()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC12() {
+    @Test
+    func puzzle12() {
         let puzzle = AOC12()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC13() {
+    @Test
+    func puzzle13() {
         let puzzle = AOC13()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC14() {
+    @Test
+    func puzzle14() {
         let puzzle = AOC14()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC15() {
+    @Test
+    func puzzle15() {
         let puzzle = AOC15()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC16() {
+    @Test
+    func puzzle16() {
         let puzzle = AOC16()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC17() {
+    @Test
+    func puzzle17() {
         let puzzle = AOC17()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC18() {
+    @Test
+    func puzzle18() {
         let puzzle = AOC18()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
-    func testAOC19() {
+    @Test
+    func puzzle19() {
         let puzzle = AOC19()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC20() {
         let puzzle = AOC20()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC21() {
         let puzzle = AOC21()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC22() {
         let puzzle = AOC22()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC23() {
         let puzzle = AOC23()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC24() {
         let puzzle = AOC24()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
     
+    @Test
     func testAOC25() {
         let puzzle = AOC25()
         
         let input1 = """
         """
         
-        XCTAssertEqual(puzzle.solve1(input: input1), 0)
+        #expect(puzzle.solve1(input: input1) == 0)
         
         let input2 = """
         """
         
-        XCTAssertEqual(puzzle.solve2(input: input2), 0)
+        #expect(puzzle.solve2(input: input2) == 0)
     }
 }
