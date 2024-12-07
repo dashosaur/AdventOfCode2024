ae685e1924e7572adf27da0bc0c47482b39fa495
Dash Brittain <roxanne.brittain@gmail.com>
2024-12-06 21:27:11 -0800

    AOC7

--- AdventOfCodeTests/PuzzleTests.swift
+++ AdventOfCodeTests/PuzzleTests.swift
@@ -158,14 +158,22 @@ struct PuzzleTests {
         let puzzle = AOC7()
         
         let input1 = """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
         """
         
-        #expect(puzzle.solve1(input: input1) == 0)
+        #expect(puzzle.solve1(input: input1) == 3749)
         
-        let input2 = """
-        """
+        let input2 = input1
         
-        #expect(puzzle.solve2(input: input2) == 0)
+        #expect(puzzle.solve2(input: input2) == 11387)
     }
     
     @Test
--- Sources/AdventOfCode/Puzzles/AOC7.swift
+++ Sources/AdventOfCode/Puzzles/AOC7.swift
@@ -11,10 +11,37 @@ struct AOC7: Puzzle {
     typealias Answer = Int
     
     func solve1(input: String) -> Int {
-        0
+        input.lines.reduceSum { line in
+            let (testValueText, numsText) = line.splitInTwo(": ")
+            let total = Int(testValueText)!
+            let nums = numsText.integers
+            return canReachTestValue(total, nums: nums, operators: [(+), (*)]) ? total : 0
+        }
     }
     
     func solve2(input: String) -> Int {
-        0
+        input.lines.reduceSum { line in
+            let (testValueText, numsText) = line.splitInTwo(": ")
+            let total = Int(testValueText)!
+            let nums = numsText.integers
+            return canReachTestValue(total, nums: nums, operators: [(+), (*), (||)]) ? total : 0
+        }
+    }
+    
+    private func canReachTestValue(_ testValue: Int, nums: [Int], operators: [(Int, Int) -> Int]) -> Bool {
+        guard nums.count > 1 else { return nums.first == testValue }
+        
+        var nums = nums
+        let a = nums.removeFirst()
+        let b = nums.removeFirst()
+        return operators.contains { op in
+            canReachTestValue(testValue, nums: [op(a, b)] + nums, operators: operators)
+        }
+    }
+}
+
+fileprivate extension Int {
+    static func || (lhs: Int, rhs: Int) -> Int {
+        Int(String(lhs) + String(rhs))!
     }
 }
